8db425addc8a64ee4218a73e08dc0b517ef33ff6
durimarr <116593065+durimarr@users.noreply.github.com>
2022-10-26 04:00:21 +0300

    Add files via upload

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-enum NumRim{I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10);
+enum NumRim{M(1000), CM(900), D(500), CD(400), C(100), XC(90), L(50), XL(40), X(10), IX(9), VIII(8), VII(7), VI(6), V(5), IV(4), III(3), II(2), I(1);
 	private int arab;
 	NumRim(int arab) {
 		this.arab = arab;
@@ -19,38 +19,20 @@ public class Test {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
 		sc.close();
-		 //String pattern2 = "(^\\d\\s[-+*/]\\s\\d$)|(^(X|IX|IV|V?I{0,3})\\s[-+/*]\\s(X|IX|IV|V?I{0,3})$)";
-		   /* Pattern p5 = Pattern.compile(pattern2);
-		    Matcher m5 = p5.matcher(input);
-		    if(!m.matches()) {
-		    	throw new InputException("Что-то пошло не так");
-		    }*/
-		String q = calc(input);
-		System.out.println("output:\n" + q);
+		
+		System.out.println("output:\n" + calc(input));
 	}
 	public static String calc(String input) throws InputException {
 		String result = null;
 		int res, i0, i2, count = 0;
 		String[] arr = input.split(" ");
 		String[] pattern = {"^\\d$", "^X|IX|IV|V?I{0,3}$"};
-		
 		if(arr.length < 3)
 			throw new InputException("a string is not a mathematical operation");
 		if(arr.length > 3)
 			throw new InputException("the format of the mathematical operation does not satisfy the task - two operands and one operator (+, -, /, *)");
 		
 		String i1 = arr[1];
-		/*String[][] regex = {{"^\\d$", "^X|IX|IV|V?I{0,3}$"}, {arr[0], arr[2]}};
-		for(int i = 0; i < regex.length; i++) {
-			for(int j = 0; j < regex[i].length; j++) {
-				Pattern p = Pattern.compile(regex[i][j]);
-				Matcher m = p.matcher(regex[i][j]);
-				if(m.matches()) {
-					System.out.println("arabic numerals " + regex[i][j]);
-					count = count + Integer.parseInt(regex[i][j]);
-				}
-			}
-		}*/
 		
 		Pattern p = Pattern.compile(pattern[0]);
 	    Matcher m = p.matcher(arr[0]);
@@ -72,10 +54,14 @@ public class Test {
 					  i0 = NumRim.valueOf(arr[0]).getNumArab();
 					  i2 = NumRim.valueOf(arr[2]).getNumArab();
 					  res = i0 + i2;
-					  //if(res > 10) result = String.valueOf(res);
-					  for(NumRim nr : NumRim.values()) 
-						if(nr.getNumArab() == res)
-					  	result = nr.name();
+					  StringBuffer sb = new StringBuffer();
+						for(NumRim nr : NumRim.values()) {
+							while(res >= nr.getNumArab()) {
+								res -= nr.getNumArab();
+								sb.append(nr.name());
+							}
+						}
+					  	result = String.valueOf(sb);
 				} 
 		break;
 		case "-":
@@ -119,10 +105,14 @@ public class Test {
 					  i0 = NumRim.valueOf(arr[0]).getNumArab();
 					  i2 = NumRim.valueOf(arr[2]).getNumArab();
 					  res = i0 * i2;
-					//if(res > 10) result = String.valueOf(res);
-					  for(NumRim nr : NumRim.values()) 
-						if(nr.getNumArab() == res)
-					  	result = nr.name();
+					  StringBuffer sb = new StringBuffer();
+						for(NumRim nr : NumRim.values()) {
+							while(res >= nr.getNumArab()) {
+								res -= nr.getNumArab();
+								sb.append(nr.name());
+							}
+						}
+					  	result = String.valueOf(sb);
 				} 
 		break;
 		default:
